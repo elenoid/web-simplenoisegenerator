d36dbdebc05f9e6ffee8281658e0c9a8dc1ed68e
elenoid <elenoiddesign@gmail.com>
2017-12-20 00:58:46 +0100

    added sound, icons

--- js/main.js
+++ js/main.js
@@ -7,8 +7,10 @@ function stopAudio(){
    var audio = document.getElementById("audio");
    audio.pause();
    $('#audio-background').css('background-image', 'url(https://cdn.pixabay.com/photo/2015/12/20/16/55/fractal-1101414_960_720.jpg)');
+   $(document.body).css('background-image', 'none');
 }
 
 function changeImage(a) {
     $('#audio-background').css('background-image', 'url('+a+')');
+    $(document.body).css('background-image', 'url('+a+')');
 }
