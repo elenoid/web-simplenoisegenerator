566d7b3c33bfc1804dcbd1af66165a40cb8da65b
elenoid <elenoiddesign@gmail.com>
2017-12-27 17:01:19 +0100

    changed pictures

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ function play(sound){
 function stopAudio(){
    var audio = document.getElementById("audio");
    audio.pause();
-   $('#audio-background').css('background-image', 'url(https://cdn.pixabay.com/photo/2015/12/20/16/55/fractal-1101414_960_720.jpg)');
+   $('#audio-background').css('background-image', 'url(../img/fractal.jpg)');
    $(document.body).css('background-image', 'none');
 }
 
 function changeImage(a) {
     $('#audio-background').css('background-image', 'url('+a+')');
-    $(document.body).css('background-image', 'url('+a+')');
+    // $(document.body).css('background-image', 'url('+a+')');
 }
